b2d5c370ab5c8f304ea4b12af685e816f0a74694
AbbieDyck <38113369+AbbieDyck@users.noreply.github.com>
2020-12-30 19:16:28 -0500

    Add files via upload

---
+++ Midterm Questions/palindrome.c
@@ -0,0 +1,50 @@
+/***************************************
+**	Developer		:Abbie Dyck		  **
+** 	Student ID		:110046150		  **
+** 	Program Name	:palindrome.c	  **
+** 	Version			:1				  **
+** 	Date			:November 14, 2020**
+****************************************/
+
+#include <stdio.h>
+
+int main(void){
+	int num;
+	int starting;
+	int reversed = 0;
+	int a;
+	int numCheck;
+
+	while(1){
+		printf("Enter an integer number: ");
+	    numCheck = scanf("%d", &num);
+
+	    while(getchar() != '\n');
+
+	    if(numCheck == 0){
+	    	continue;
+	    }
+
+	    if(num == 0){
+	    	puts("Goodbye!");
+	        break;
+	    }else if(num < 0){
+	    	num *= -1;
+	    }
+
+	    starting = num;
+	    reversed = 0;
+
+	    while(num != 0){
+	    	a = num % 10;
+	        reversed = reversed * 10 + a;
+	        num = num / 10;
+	    }
+
+	    if(starting != reversed){
+	    	printf("%d is not a Palindrome.\n", starting);
+	    }else{
+		    printf("%d is a Palindrome.\n", starting);
+	    }
+	}
+}//End main()
---
+++ Midterm Questions/sequence.c
@@ -0,0 +1,49 @@
+/***************************************
+**	Developer		:Abbie Dyck		  **
+** 	Student ID		:110046150		  **
+** 	Program Name	:sequence.c	 	  **
+** 	Version			:1				  **
+** 	Date			:November 14, 2020**
+****************************************/
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+	int num;
+	int factorial = 1;
+	int try = 1;
+	double total = 0;
+
+	   while(try == 1){
+	      printf("Please enter a positive integer value (between 1 and 7, inclusive): ");
+	      scanf(" %d", &num);
+
+	      if(num <= 0 || num >= 8){
+	         puts("Invalid input.");
+	         continue;
+	      }
+
+	      for(int x = 2; x <= (num+1); x++){
+	         for(int y = 1; y <= x; y++){
+	            factorial *= y;
+	         }
+
+	         total = total + 1/(double)factorial;
+
+	         if(factorial == 2){
+	        	 printf("1/2 ");
+	         }else{
+	        	 printf("+ 1/%d ", factorial);
+	         }
+	         factorial = 1;
+	      }
+
+	      printf("= %.3f\n", total);
+	      total = 0;
+	      printf("Would you want to try again? (1/yes, 0/no) ");
+	      scanf("%d", &try);
+	   }
+	   puts("Goodbye!");
+}//End main()
